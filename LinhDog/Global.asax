8911fb1a02838c1673eeeeb313c5104910ed1a06
Nhan Đặng Minh Đạt <dat.ndm.15dh110347@outlook.com>
2018-10-16 10:29:47 +0700

    Hello

---
+++ LinhDog/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="LinhDog.MvcApplication" Language="C#" %>
---
+++ LinhDog/Models/Blog.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+
+namespace LinhDog
+{
+    public class Blog
+    {
+        public int BlogID { get; set; }
+        public string Name { get; set; }
+    }
+    public class BloggingContext: DbContext
+    {
+        public BloggingContext() : base("Default")
+        { }
+        public DbSet<Blog> Blogs { get; set; }
+    }
+}
